2a93c08b2084fdfa0cafde9539bb1798db6fbc42
AsjadIftikhar <iftikharasjad@gmail.com>
2020-05-22 05:04:54 +0500

    Asjads Version Complete

--- CricPanda/App_Start/RouteConfig.cs
+++ CricPanda/App_Start/RouteConfig.cs
@@ -14,9 +14,14 @@ namespace CricPanda
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Teaminfo",
+                url: "{controller}/{action}/{id}",
+                defaults: new { Controller = "Users", action = "TeamInfo", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CricPanda/Models/Tournaments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CricPanda.Models
+{
+    public class Tournaments
+    {
+        public int Tid { get; set; }
+        public string Name { get; set; }
+        public string loc { get; set; }
+        public string s_date  { get; set; }
+        public string e_Date { get; set; }
+    }
+}
